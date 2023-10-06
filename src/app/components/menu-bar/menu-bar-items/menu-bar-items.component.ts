ec0a9bc20e2bf6249e890c5f9dd122a752a63fac
gustavomotamacedo <mota.macedo05@gmail.com>
2023-10-06 12:56:05 -0300

    update menu-bar component

---
+++ src/app/components/menu-bar/menu-bar-items/menu-bar-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar-items',
+  templateUrl: './menu-bar-items.component.html',
+  styleUrls: ['./menu-bar-items.component.css']
+})
+export class MenuBarItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
